4794156948f4dc34c8f4be7113f1fbf37e437cab
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-10-15 10:53:47 -0400

    Removed draft charting js files.  Renamed main charting js file.

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,7 +1,8 @@
 function init() {
+    // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
-
-    d3.json("samples.json").then((data) => {
+    // Use the list of sample names to populate the select options
+    d3.json("./static/js/samples.json").then((data) => {
         console.log(data);
         var sampleNames = data.names;
         sampleNames.forEach((sample) => {
@@ -13,18 +14,22 @@ function init() {
     })
 };
 
+// Fetch new data each time a new sample is selected
 function optionChanged(newSample) {
     buildMetadata(newSample);
     buildCharts(newSample);
 };
 
+// Demographics Panel 
 function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
+    d3.json("./static/js/samples.json").then((data) => {
         var metadata = data.metadata;
+        // Filter the data for the object with the desired sample number
         var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
         var result = resultArray[0];
+        // Use d3 to select the panel with id of `#sample-metadata`
         var PANEL = d3.select("#sample-metadata");
-
+        // Clear any existing metadata
         PANEL.html("");
         PANEL.append("h6").text("ID: " + result.id);
         PANEL.append("h6").text("ETHNICITY: " + result.ethnicity);
@@ -36,5 +41,5 @@ function buildMetadata(sample) {
     });
 };
 
-
+// Initialize the dashboard
 init();
